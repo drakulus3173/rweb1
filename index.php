0c7e83fa48cad01aaf886fa4a7fba86e268396ac
drakulus3173 <drakulus3173@hotmail.com>
2016-05-04 11:20:48 +1200

    V3

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ruzz Site</title>
-        <link rel="stylesheet" type="text/css" href="rw/style.css">
-    </head>
-    <body>
-        <p><a href="rw/page1.htm">\(-.-\)CLICK ME(/-.-)/</a></p>
-    </body>
-</html>
+$ git init
+$ heroku git:remote -a "name of heroku app"
+
+
+Commit your code to the repository and deploy it to Heroku using Git.
+
+$ git add .
+$ git commit -am "make it better"
+$ git push heroku master
+$ heroku open
