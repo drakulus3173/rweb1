3060e259a4890327c67d76774c03e414d1fce8c9
drakulus3173 <drakulus3173@hotmail.com>
2016-05-11 15:36:49 +1200

    Add files via upload

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
   <meta charset="utf-8"/>
   
   <!-- BootStrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-
-
-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
   
   <!-- CSS Script -->
@@ -19,16 +17,17 @@
 
   <header class = "container">
 		<div class = "row">
-      <h1 class="col-sm-4">Anointed Flyff</h1>
+      <h1 class="col-sm-4 animated infinite flash"> Anointed Flyff </h1>
       <nav class="col-sm-8 text-right">
-        <p class="button" style="vertical-align:middle" href="rw/page2.htm"><span> Home </span></a></p>
-      	<p class="button" style="vertical-align:middle"><span> Item Mall </span></a></p>
-      	<p class="button" style="vertical-align:middle"><span> Server Info </span></a></p>
-      	<p class="button" style="vertical-align:middle"><span> Ranking </span></a></p>
-      	<p class="button" style="vertical-align:middle"><span> Forum </span></a></p>
+        <a class="button" style="vertical-align:middle" href="index.php"><span> Home </span> </a>
+      	<a class="button" style="vertical-align:middle" href="rw/page1.htm"><span> Item Mall </span> </a>
+      	<a class="button" style="vertical-align:middle" href="rw/page2.htm"><span> Server Info </span> </a>
+      	<a class="button" style="vertical-align:middle" href="rw/page3.htm"><span> Ranking </span> </a>
+      	<a class="button" style="vertical-align:middle" href="https://www.facebook.com/"><span> Forum </span></a>
       </nav>
     </div>
   </header>
+
   <section class="jumbotron">
 	<div class="container">
 		
@@ -39,15 +38,9 @@
   <div class="row">
     <p class="col-sm-4">&copy; Anointed Flyff 2016</p>
     <ul class="col-sm-8">
-    <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
-
-4/twitter.svg"></li>
-  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
-
-4/facebook.svg"></li>
-  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
-
-4/instagram.svg"></li>
+    <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/twitter.svg"></li>
+  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/facebook.svg"></li>
+  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/instagram.svg"></li>
   <li class="col-sm-1"><li class="col-md-1">
   <img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/medium.svg">
 </li></li>
