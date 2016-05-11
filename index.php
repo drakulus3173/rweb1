091aa90719f2ec2485922ff585b77f85493c1dfe
drakulus3173 <drakulus3173@hotmail.com>
2016-05-11 12:53:57 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-    <h1><a href="rw\page1.htm">CLICK ME!!!</a></</h1>
-  </body>
+<html>
+<head>
+  <title>Anointed Flyff</title>
+  <meta charset="utf-8"/>
+  
+  <!-- BootStrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-
+
+1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+  
+  <!-- CSS Script -->
+  <link rel="stylesheet" type="text/css" href="rw/styles.css">
+  <link href="rw/animate.min.css" type="text/css" rel="stylesheet">
+  
+</head>
+<body>
+
+  <header class = "container">
+		<div class = "row">
+      <h1 class="col-sm-4">Anointed Flyff</h1>
+      <nav class="col-sm-8 text-right">
+        <p class="button" style="vertical-align:middle" href="rw/page2.htm"><span> Home </span></a></p>
+      	<p class="button" style="vertical-align:middle"><span> Item Mall </span></a></p>
+      	<p class="button" style="vertical-align:middle"><span> Server Info </span></a></p>
+      	<p class="button" style="vertical-align:middle"><span> Ranking </span></a></p>
+      	<p class="button" style="vertical-align:middle"><span> Forum </span></a></p>
+      </nav>
+    </div>
+  </header>
+  <section class="jumbotron">
+	<div class="container">
+		
+  	</div>
+  </section>
+  
+  <footer class="container">
+  <div class="row">
+    <p class="col-sm-4">&copy; Anointed Flyff 2016</p>
+    <ul class="col-sm-8">
+    <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
+
+4/twitter.svg"></li>
+  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
+
+4/facebook.svg"></li>
+  <li class="col-sm-1"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-
+
+4/instagram.svg"></li>
+  <li class="col-sm-1"><li class="col-md-1">
+  <img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/medium.svg">
+</li></li>
+    </ul>
+  </div>
+</footer>
+    
+</body>
 </html>
